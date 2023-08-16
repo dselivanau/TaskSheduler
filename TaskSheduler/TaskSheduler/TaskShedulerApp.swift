8e8e66912d6a78955b3ddbfc3860afa5de1fe811
Denis Selivanov <innowise@innowises-Mac-mini.local>
2023-08-16 07:59:52 +0200

    initial commit with the app

---
+++ TaskSheduler/TaskSheduler/TaskShedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskShedulerApp.swift
+//  TaskSheduler
+//
+//  Created by innowise on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskShedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
